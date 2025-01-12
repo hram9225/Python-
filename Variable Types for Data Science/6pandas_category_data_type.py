f99b5e98372774c4b4d88f372eb4a2cb811f4869
Houston <123344744+hram9225@users.noreply.github.com>
2025-01-11 22:17:06 -0500

    Create 6pandas_category_data_type.py

---
+++ Variable Types for Data Science/6pandas_category_data_type.py
@@ -0,0 +1,21 @@
+#The pandas .Categorical() method can be used to store data as type category and indicate the order of the categories.
+import codecademylib3
+
+# Import pandas with alias
+import pandas as pd
+
+# Import dataset as a Pandas Dataframe
+movies = pd.read_csv('netflix_movies.csv')
+
+# View the first five rows of the dataframe
+print(movies.head())
+
+# Print the unique values of the rating column
+print(movies['rating'].unique())
+
+# Change the data type of `rating` to category
+movies['rating'] = pd.Categorical(movies['rating'], ['NR', 'G', 'PG', 'PG-13', 'R'], ordered=True)
+
+# Recheck the values of `rating` with .unique()
+
+print(movies['rating'].unique())
