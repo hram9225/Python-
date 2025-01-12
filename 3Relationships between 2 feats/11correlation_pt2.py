ee5c3fc4bf7d57375226a6c0472b4930daeeabe9
Houston <123344744+hram9225@users.noreply.github.com>
2025-01-12 16:12:42 -0500

    Update 11correlation_pt2.py

--- 3Relationships between 2 feats/11correlation_pt2.py
+++ 3Relationships between 2 feats/11correlation_pt2.py
@@ -1,1 +1,16 @@
 #limitations when using correlation or covariance since they measure strngth with linear relationships, not other types like quadratic
+import pandas as pd
+import matplotlib.pyplot as plt 
+import codecademylib3
+from scipy.stats import pearsonr
+
+sleep = pd.read_csv('sleep_performance.csv')
+
+# create scatter plot
+plt.scatter(sleep.hours_sleep, sleep.performance)
+plt.show()
+
+# calculate the correlation for `hours_sleep` and `performance`:
+corr_sleep_performance, p = pearsonr(sleep.hours_sleep, sleep.performance)
+print(corr_sleep_performance)
+
