fb3110bfbbbec34a7be39d3c7ac7ae638a9fcf97
Houston <123344744+hram9225@users.noreply.github.com>
2025-01-12 14:31:16 -0500

    Create 1intro.py

---
+++ 3Relationships between 2 feats/1intro.py
@@ -0,0 +1,17 @@
+#learning to assess associations between categorical and quantitative variables
+
+import numpy as np
+import pandas as pd
+import codecademylib3
+
+students = pd.read_csv('students.csv')
+
+#print the first five rows of students:
+print(students.head())
+
+#separate out scores for students who live in urban and rural locations:
+scores_urban = students.G3[students.address == 'U']
+scores_rural = students.G3[students.address == 'R']
+
+print(scores_urban)
+print(scores_rural)
